2f10961b91784d412003a92335ade7cb6d1a7dd3
taniachris8 <dove6464dove@gmail.com>
2025-07-06 13:34:26 +0300

    Project completed

--- public/homePage.js
+++ public/homePage.js
@@ -1,9 +1,106 @@
 const logoutButton = new LogoutButton();
 
-logoutButton.action = () => { 
-    ApiConnector.logout((response) => { 
-        if (response.success) { 
-            location.reload();
-        }
-    });
-}
+logoutButton.action = () => {
+  ApiConnector.logout((response) => {
+    if (response.success) {
+      location.reload();
+    }
+  });
+};
+
+ApiConnector.current((response) => {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
+
+const ratesBoard = new RatesBoard();
+
+const getStocks = () => {
+  ApiConnector.getStocks((response) => {
+    if (response.success) {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }
+  });
+};
+
+getStocks();
+setInterval(() => getStocks(), 60000);
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Счёт был успешно пополнен");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Конвертация произведена успешно");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Перевод средств совершен успешно");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites((response) => {
+  if (response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  }
+});
+
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      favoritesWidget.setMessage(
+        true,
+        "Пользователь был успешно добавлен в список избранных"
+      );
+    } else {
+      favoritesWidget.setMessage(false, response.error);
+    }
+  });
+};
+
+favoritesWidget.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      favoritesWidget.setMessage(
+        true,
+        "Пользователь был успешно удалён из списока избранных"
+      );
+    } else {
+      favoritesWidget.setMessage(false, response.error);
+    }
+  });
+};
--- public/loginPage.js
+++ public/loginPage.js
@@ -4,9 +4,7 @@ const userForm = new UserForm();
 
 userForm.loginFormCallback = (data) => {
   ApiConnector.login(data, (response) => {
-    console.log(response);
     if (response.success) {
-      console.log(data);
       location.reload();
     } else {
       userForm.setLoginErrorMessage(response.error);
@@ -16,9 +14,7 @@ userForm.loginFormCallback = (data) => {
 
 userForm.registerFormCallback = (data) => {
   ApiConnector.register(data, (response) => {
-    console.log(data);
     if (response.success) {
-      console.log("You are registered");
       location.reload();
     } else {
       userForm.setRegisterErrorMessage(response.error);
